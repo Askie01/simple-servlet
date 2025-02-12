9329e7bb44c286de24e6747d9c1f366024203758
Adam Pawłowski <61596@itcinfotech.com>
2025-02-12 14:10:55 +0100

    Update 'HelloServlet'
    * Updated the mentioned servlet class to see how different type of HTTP requests are handled.

--- src/main/java/com/example/servlettutorial/HelloServlet.java
+++ src/main/java/com/example/servlettutorial/HelloServlet.java
@@ -1,22 +1,34 @@
 package com.example.servlettutorial;
 
-import java.io.*;
-
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
+    @Override
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
             throws ServletException, IOException {
+        log.atInfo().log("/hello GET request has been received.");
+        response.setContentType("text/html");
+        final PrintWriter printWriter = response.getWriter();
+        printWriter.println("Thanks for sending the GET request.");
+        printWriter.println("Much thanks for the data :)");
     }
 
     @Override
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
             throws ServletException, IOException {
+        log.atInfo().log("/hello POST request has been received.");
     }
 }
